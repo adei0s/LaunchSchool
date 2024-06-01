dbf27c30306010e9bebb16f92f34635f13456448
Tingting Li <ting2art@gmail.com>
2024-06-01 22:36:45 +0000

    previous small_problems started

---
+++ PY110/PY110_small_problems/easy_1/searching101.py
@@ -0,0 +1,15 @@
+num1 = input("Enter the 1st number: ")
+num2 = input("Enter the 2nd number: ")
+num3 = input("Enter the 3rd number: ")
+num4 = input("enter the 4th number: ")
+num5 = input("enter the 5th number: ")
+num6 = input("enter the 6th number: ")
+
+lst = [num1, num2, num3, num4, num5]
+
+if num6 in lst:
+    print(f'{num6} is in {",".join(lst)}')
+else:
+    print(f'{num6} isn\'t in {",".join(lst)}')
+    
+
